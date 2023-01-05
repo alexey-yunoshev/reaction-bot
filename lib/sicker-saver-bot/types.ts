2944ee3d9762736c0f9eeb1b7be56ad82dec107d
Alexey Yunoshev <alexey.yunoshev@gmail.com>
2023-01-05 22:06:35 +0400

    Add saver sticker bot

---
+++ lib/sicker-saver-bot/types.ts
@@ -0,0 +1,20 @@
+export const collections = {
+    'сделалдело': '1',
+    'радость': '1',
+    'грусть': '2',
+    'пиздец': '3',
+}
+
+/**
+ * A collection of unassigned stickers
+ */
+export const UNASSIGNED_COLLECTION_ID = '0';
+
+export interface ReactionSticker {
+    // Can be sent. Not unique
+    fileId: string;
+    // Cannot be sent. Unique
+    fileUniqueId: string;
+    collectionId: string;
+    setName: string;
+}
